ecf3a61ac61987df32ed09b78a9bc28b209eb640
Maisy Capps <maisycapps@gmail.com>
2024-08-07 11:30:50 -0700

    continued work

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // STATE
-class freelancer {
-    constructor (name, occupation, startingPrice){
-    name = this.name,
-    occupation = this.occupation,
-    startingPrice = this.startingPrice
-    }
+const freelancers = [
+    {name: "Alice", occupation: "writer", startingPrice: 30},
+    {name: "Bob", occupation: "teacher", startingPrice: 50}
+];
+
+render();
+
+function render(){
+// render names
+    const nameList= document.querySelector("#names");
+    const nameSrc = freelancers.map((profile) => {
+        const spec = document.createElement("td");
+        spec.classList.add(profile.name);
+        return spec;
+    }) 
+    nameList.replaceChildren(...nameSrc);
+// render occupations
+
+// render starting prices
+
+// update av price
+    // const prices = document.querySelector("#startingPrice");
+    // const updatedAv = (Math.max(...prices))-(Math.min(...prices))
+    // average.replace(`The average starting price is ${updatedAv}`)
 }
-
-const alice = new freelancer("Alice", "writer", "30");
-console.log(alice);
-
