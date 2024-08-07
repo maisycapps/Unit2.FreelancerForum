adf855e77f504e0e8c566af51292f94221a38169
Maisy Capps <maisycapps@gmail.com>
2024-08-07 10:02:11 -0700

    added files

---
+++ index.js
@@ -0,0 +1,12 @@
+// STATE
+class freelancer {
+    constructor (name, occupation, startingPrice){
+    name = this.name,
+    occupation = this.occupation,
+    startingPrice = this.startingPrice
+    }
+}
+
+const alice = new freelancer("Alice", "writer", "30");
+console.log(alice);
+
